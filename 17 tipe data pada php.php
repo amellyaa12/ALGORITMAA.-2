c61851257ecee452a746b4d32741686b4ec566bc
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 19:17:57 +0700

    data pada php

---
+++ 17 tipe data pada php.php
@@ -0,0 +1,28 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'p';
+
+// tipe data string (teks)
+$nama_lengkap = "Amellya";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 50;
+
+// tipe data float
+$tinggi = 158;
+
+// tipe data boolean
+$gadis = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "gadis : $gadis";
+
+echo "<br><br><br><strong>By Amellya </strong>";
