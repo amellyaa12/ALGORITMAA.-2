c7a9d6b5b89378010a106c529c09940e4b3a8b77
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 19:02:47 +0700

    nilai variabel

---
+++ 11 mencetak nilai variabel pada php.php
@@ -0,0 +1,10 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "Gula Merah";
+$harga = 15000;
+
+// menampilkan isi variabel
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+echo "<br><br><br><strong>By Amellya </strong>";
