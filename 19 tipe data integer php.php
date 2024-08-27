21a5761be4f37c86ddaf5dc318f1c5cec08cb557
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 19:22:12 +0700

    data integer

---
+++ 19 tipe data integer php.php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 44; // angka positif
+$poin = -33; // angka negatif
+
+echo "angka positif: $nilai";
+echo "angka negatif: $poin";
+
+echo "<br><br><br><strong>By Amellya </strong>";
