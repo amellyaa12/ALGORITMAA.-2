0a68b48f567e931fb43c419113d0b4dbfb903d48
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 19:24:24 +0700

    data float

---
+++ 20 tipe data float php.php
@@ -0,0 +1,9 @@
+<?php
+$waktu = 230.12;
+$kecepatan = 1325.12;
+$jarak = 230.12 * 13235.12 ;
+
+// batasi angka di belakang koma
+echo "<p>nilai jarak: $jarak";
+
+echo "<br><br><br><strong>By Amellya </strong>";
