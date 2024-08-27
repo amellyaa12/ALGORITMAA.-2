db0d02aa60bf2d760d6ad353d7ebeb9c51f7fcd1
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 18:44:27 +0700

    penulisan case php

---
+++ 5 aturan penulisan case php.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "Amellya";
+$NAMA = "Amel";
+$umur =16;
+const INI_KONSTANTA = 123;
+$ini_variabel =25;
+$iniJugaVariabel = 50;
+$iniJugaVariabel;
+
+echo "====== mencetak variabel ====== ";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama panggilan saya adalah $NAMA";
+echo "<p>Umur saya adalah $umur";
+
+echo
+   
+    "<p>Coba pakai blok program",
+    "<p>Nama saya adalah $nama",
+    "<p>Nama panggilan saya adalah $NAMA",
+    "<P>Umur saya adalah $umur";
+
+    echo "<br><br><br><srong>By Amellya </strong>";
+
+?>
