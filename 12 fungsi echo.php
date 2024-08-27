20594d4cc760d4c4a1df444f942c8d1410834d68
amellyaa12 <lyaaamel4@gmail.com>
2024-08-27 19:05:32 +0700

    fungsi echo

---
+++ 12 fungsi echo.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello Amellya!<br>");
+echo "Disedang belajar PHP!<br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
+echo "<br><br><br><strong>By Amellya </strong>";
+?>
